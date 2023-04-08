fd2238515200646a97edd5a4deaf433299e76955
George Chokhonelidze <g_chokhonelidze@outlook.com>
2023-04-08 10:11:12 -0500

    example of server

--- src/app.py
+++ src/app.py
@@ -18,4 +18,17 @@ def githun():
     os.system("az login -u "+username+" -p "+password)
     os.system("az webapp create  --resource-group "+resourceGroup+" --plan  "+ plan +"--name "+branch+" ---multicontainer-config-type compose --multicontainer-config-file /usr/src/app/src/ALLINONE/docker-compose.yml")
     return "ok"
-    
+
+@app.route('/github_delete',methods=['POST'])
+def github_delete():
+    obj = request.json
+    print(obj,flush=True)
+    branch = obj['ref'].split('/')[-1]
+    resourceGroup = os.environ['RESOURCE_GROUP']
+    username= os.environ['USERNAME']
+    password= os.environ['PASSWORD']
+
+    os.system("az login -u "+username+" -p "+password)
+    os.system("az webapp delete --name"+branch+"--resource-group "+resourceGroup)
+    os.system("cd /usr/src/app/src/ALLINONE/ && git checkout dev && git pull && git submodule update")
+    return "ok"
